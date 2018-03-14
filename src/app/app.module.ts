867d8a1bbb76055514cdc491bc482bdae696d7c2
Jonas De Vrient <jonas.devrient.y0029@student.hogent.be>
2018-03-14 19:47:54 +0100

    Add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { PinboardListComponent } from './pinboard/pinboard-list/pinboard-list.component';
 import { PinboardService } from './services/pinboard.service';
 import { HeaderComponent } from './shared/header/header.component';
 
+import { RouterModule, Routes, Router } from '@angular/router';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
+import { PinboardDetailComponent } from './pinboard/pinboard-detail/pinboard-detail.component';
+
+
+const appRoutes: Routes = [
+  { path: 'prikborden', component: PinboardListComponent },
+  { path: '', redirectTo: '/prikborden', pathMatch: 'full' },
+  { path: 'prikbord/:stad', component: PinboardDetailComponent },
+  { path: '**', component: PageNotFoundComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     PinboardListComponent,
-    HeaderComponent
+    HeaderComponent,
+    PageNotFoundComponent,
+    PinboardDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } //debugging purpose only
+    )
   ],
   providers: [PinboardService],
   bootstrap: [AppComponent]
