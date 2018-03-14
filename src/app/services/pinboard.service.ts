563afed0da1a0ea719b8347f44b7769dca833c7d
Jonas De Vrient <jonas.devrient.y0029@student.hogent.be>
2018-03-14 19:50:48 +0100

    Change pinboard-service

--- src/app/services/pinboard.service.ts
+++ src/app/services/pinboard.service.ts
@@ -1,24 +1,41 @@
 import { Injectable } from '@angular/core';
 import { PinBoard } from '../models/pin-board.model';
 import { Location } from '../models/location.model';
+import { Post } from '../models/post.model';
 
 @Injectable()
 export class PinboardService {
   private pinboards: PinBoard[];
 
-  constructor() { 
+  constructor() {
+    let pinboardWithPosts = new PinBoard('Gent', new Location(4,6));
+    pinboardWithPosts.addPost(new Post('Eerste Post!', 'Lorem ipsum naturrlijk'))
+    pinboardWithPosts.addPost(new Post('Tweede titel zeg!', 'lorem ipsum twee natuurlijk'));
     this.pinboards = [
-      new PinBoard('Gent', new Location(4, 6)),
+      pinboardWithPosts,
       new PinBoard('Oudenaarde', new Location(888.9, 789.2)),
       new PinBoard('Dendermonde', new Location(98, 99))
     ]
+
+    
   }
 
-  getPinboards():PinBoard[]{
+  getByCityName(cityName: string): PinBoard {
+    let foundPinboard = this.pinboards
+      .find(pb => pb.city.trim().toLocaleLowerCase() === cityName.trim().toLocaleLowerCase());
+
+    if (foundPinboard != undefined) {
+      return foundPinboard;
+    } else {
+      throw new Error('Geen borden in deze stad!');
+    }
+  }
+
+  getAll(): PinBoard[] {
     return this.pinboards;
   }
 
-  addPinboard(pinboard: PinBoard){
+  addPinboard(pinboard: PinBoard) {
     this.pinboards.push(pinboard);
   }
 
